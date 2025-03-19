5882d5d369fe5fd5793c3cb1111169d5cecfc2d9
RachelShachar <rachels@kkl.org.il>
2025-03-19 15:29:43 +0200

    init

--- obj/Debug/net9.0/ServerProjectMvcWithoutView.AssemblyInfo.cs
+++ obj/Debug/net9.0/ServerProjectMvcWithoutView.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerProjectMvcWithoutView")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56b30371ba7223407c93f92bffc4ec518c9e1ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerProjectMvcWithoutView")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerProjectMvcWithoutView")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
